76b6278444a4a3bf52e06eddb1e216cfdee839fe
Daivashala Dhepe <daivapatil7@gmail.com>
2022-11-08 09:42:05 +0530

    now start typescript
    now start typescript

---
+++ variables.js
@@ -0,0 +1,7 @@
+var n1 = 10;
+console.log(n1); //compile with js because browser understand only js.
+console.warn("-----------------------------------------");
+var s1 = "You are the creator of your destiny";
+var s2 = " Powerful people come from powerful places";
+console.log(s1);
+console.log(s2);
---
+++ variables.ts
@@ -0,0 +1,10 @@
+var n1:number = 10;
+console.log(n1);  //compile with js because browser understand only js.
+console.warn("-----------------------------------------");
+
+var s1 : string = "You are the creator of your destiny";
+var s2: string= " Powerful people come from powerful places";
+console.log(s1);
+console.log(s2);
+
+
